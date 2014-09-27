a65599631a556ecdd57b0e52c5ebbbb64ab6fe3f
Brandon Bischof <bbichof.alt@gmail.com>
2014-09-27 16:34:53 -0400

    tested again

--- gittest/src/gittest/hi.java
+++ gittest/src/gittest/hi.java
@@ -2,7 +2,8 @@ package gittest;
 
 public class hi {
 	public static void main(String[] args){
-		System.out.println("okay");
+		System.out.println("Yeah");
+		System.out.println("what");
 	}
 //hi
 }
